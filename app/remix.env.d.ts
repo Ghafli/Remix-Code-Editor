9ea59a007939f297f6dfa0055c5f159f55a502c5
Ahmed Ghafli <133090288+Ghafli@users.noreply.github.com>
2024-12-20 18:06:43 +0300

    Update remix.env.d.ts

--- app/remix.env.d.ts
+++ app/remix.env.d.ts
@@ -2,9 +2,8 @@
 /// <reference types="@cloudflare/workers-types" />
 
 module.exports = {
-  future: {
-    v3_relativeSplatPath: true,
-    v3_singleFetch: true,
-    v3_throwAbortReason: true,
-  },
+  serverDependenciesToBundle: [
+    "nanostores/react",
+    "@webcontainer/api",
+  ],
 };
